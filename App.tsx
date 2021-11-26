502306edb55ed7b3276aba89c9f38b3b9586d5bf
Aron Berezkin <aron.berezkin@gmail.com>
2021-11-26 15:01:36 +0100

    chore: initial setup, assets

--- App.tsx
+++ App.tsx
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import introImage from "./assets/introImage.png";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      SplashScreen.hideAsync();
+    }, 1500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Image source={introImage} />
+        <Text style={styles.text}>Welcome!</Text>
+      </View>
+      <StatusBar style="dark" translucent backgroundColor="transparent" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    position: "absolute",
+    bottom: 80,
+    left: 40,
+    fontSize: 36,
+    color: "white",
   },
 });
